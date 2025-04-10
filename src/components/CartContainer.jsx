3b2e2390dcabdff3d4552282cdec32c348ba4c3c
lucianamg <luciana.exe@gmail.com>
2025-04-11 01:16:35 +0200

    subida a firebase

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,20 +1,53 @@
 import { useContext } from "react";
 import { context } from "./CustomContext";
 import Cart from "./Cart";
-import Form from "./Form";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import app from "../firebaseConfig";
+
 
 export const CartContainer = () => {
-  const { cart, removeFromCart, clearCart } = useContext(context);
-  const total = cart.reduce((sum, item) => sum + item.precio, 0);
-  const handleBuy = () => {
-    const form = document.getElementById("purchaseForm");
-    if (!form.checkValidity()) {
-      form.reportValidity(); // muestra los mensajes de error del navegador
-      return;
-    }
-    alert("¡Gracias por tu compra! Nos contactaremos al correo indicado.");
-    form.reset();
-    clearCart();
+    const { cart, removeFromCart, clearCart } = useContext(context);
+    const total = cart.reduce((sum, item) => sum + item.precio, 0);
+    const handleBuy = () => {
+        const form = document.getElementById("purchaseForm");
+        if (!form.checkValidity()) {
+        form.reportValidity(); // muestra los mensajes de error del form
+        return;
+        }
+        alert("¡Gracias por tu compra! Nos contactaremos al correo indicado.");
+        form.reset();//borro form post compra
+
+        const db = getFirestore(app); // para mandar la info a firebase
+        const cartDetailCollection = collection(db, "CartDetail");
+
+        const formData = {
+            email: form.elements["email"].value,
+            nombre: form.elements["name"].value,
+            direccion: form.elements["address"].value,
+            tarjeta: form.elements["card"].value,
+            vencimiento: form.elements["expiration"].value,
+            cvc: form.elements["cvcCode"].value,
+          };
+        
+        const cartData = {
+            totalItems: cart.length,
+            totalPrice: total,
+            items: cart.map((item) => item.nombre),
+            datosUsuario: formData,//aca agrego lo del form
+            timestamp: new Date()
+        };
+
+        console.log("Datos a guardar en Firestore:", cartData);
+
+        addDoc(cartDetailCollection, cartData)
+        .then((docRef) => {
+            console.log("Compra registrada con ID:", docRef.id);
+            
+        })
+        .catch((error) => {
+            console.error("Error al guardar la compra:", error);
+        }); // fin de mandar la info a firebase
+        clearCart();//borro carrito post compra
     };
 //como el form está dentro del mismo Cart y no separado del flujo, 
 //también desaparece el form al vaciarse el carrito. Me salio de casualidad.
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,17 +6,17 @@ export const Form = () => {
             <div className="purchaseFormContent"> 
                 <form id="purchaseForm" className="purchaseForm">
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" className="formField" placeholder="Ingresa tu email para que podamos contactarte" required />
+                    <input name="email" type="email" className="formField" placeholder="Ingresa tu email para que podamos contactarte" required />
                     <label htmlFor="name">Nombre</label>
-                    <input type="text" className="formField" placeholder="Ingresa tu nombre como figura en la tarjeta" required/>
+                    <input type="text" name="name" className="formField" placeholder="Ingresa tu nombre como figura en la tarjeta" required/>
                     <label htmlFor="address">Dirección de envío</label>
-                    <textarea className="formField" placeholder="Escribe tu dirección completa..." minLength={40} required></textarea>
+                    <textarea className="formField" name="address" placeholder="Escribe tu dirección completa..." minLength={40} required></textarea>
                     <label htmlFor="tarjeta">Datos de la tarjeta</label>
-                    <textarea className="formField" placeholder="Escribe los 16 numeros de la tarjeta..." minLength={15} required></textarea>
+                    <textarea className="formField" name="card" placeholder="Escribe los 16 numeros de la tarjeta..." minLength={15} required></textarea>
                     <label htmlFor="vencimiento">Vencimiento</label>
-                    <textarea className="formField" placeholder="Escribe la fecha de vencimiento como MMAA" minLength={4} required></textarea>
+                    <textarea className="formField" name="expiration" placeholder="Escribe la fecha de vencimiento como MMAA" minLength={4} required></textarea>
                     <label htmlFor="CVC">Codigo que figura en el dorso</label>
-                    <textarea className="formField" placeholder="Escribe el codigo de 3 o 4 numeros que figura en el dorso" minLength={3} required></textarea>
+                    <textarea className="formField" name="cvcCode" placeholder="Escribe el codigo de 3 o 4 numeros que figura en el dorso" minLength={3} required></textarea>
                 </form>
             </div>
         </div>
